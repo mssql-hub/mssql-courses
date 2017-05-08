961f8522cbef81c0dd8666231c1c2c9ae3183b86
İSMAİL TAŞDELEN <ismailtasdelen@users.noreply.github.com>
2017-05-08 13:40:04 +0300

    master

---
+++ mssql_db/ismailtasdelen_19.sql
@@ -0,0 +1,7 @@
+SELECT S.U_No, COUNT(DISTINCT M_No) Mus_Sayi, SUM(Miktar) TopMiktar FROM SIPARIS S GROUP BY S.U_No
+
+SELECT S.U_No, U.UrunAdi, COUNT(DISTINCT M_No) Mus_Sayi, SUM(Miktar) TopMiktar 
+		FROM SIPARIS S, URUN U WHERE S.U_No=U.UNo GROUP BY S.U_No, U.UrunAdi 
+
+SELECT * FROM URUN WHERE EldekiStok <50
+UPDATE URUN SET BirimFiyat = BirimFiyat * 1.1 WHERE EldekiStok < 50
